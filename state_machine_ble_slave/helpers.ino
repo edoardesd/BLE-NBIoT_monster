a911f3632e5ae24f201605f33b09623ec5fec471
edoardesd <edoardesd@gmail.com>
2022-12-06 15:20:50 +0100

    add slave advertiser with name

---
+++ state_machine_ble_slave/helpers.ino
@@ -0,0 +1,86 @@
+void readSerial(){
+  if (Serial.available()) {
+    String str = "";
+    Serial.print("Input: ");
+    
+    prevMillis = millis();
+    while (millis() - prevMillis < READ_TIME) {
+      if (Serial.available()) {
+        char c = Serial.read();
+        if (c != 10 && c != 13) { // Don't send line end characters to HM10.
+          str += c;
+        }
+      }
+    }
+
+    if (strstr(str.c_str(), BLE_TAG)){
+      Serial.print("BLE Stream: ");
+      str = strremove(str.c_str(), BLE_TAG);
+      BLESerial.print(str);
+    }
+
+    if (strstr(str.c_str(), NBIOT_TAG)){
+      Serial.print("NB-IoT Stream: ");
+      str = strremove(str.c_str(), NBIOT_TAG);
+      str = strcat(str.c_str(), "\r\n");
+      NBIOTSerial.write(str.c_str());
+    }
+    Serial.println(str);
+  }
+}
+
+char *strremove(char *str, const char *sub) {
+    size_t len = strlen(sub);
+    if (len > 0) {
+        char *p = str;
+        while ((p = strstr(p, sub)) != NULL) {
+            memmove(p, p + len, strlen(p + len) + 1);
+        }
+    }
+    return str;
+}
+
+void readNBIOT(){
+  if(NBIOTSerial.available()){ 
+    outputNBIOT=NBIOTSerial.readStringUntil('\n');
+    Serial.println(outputNBIOT);
+  }
+}
+
+
+void checkOkBLE(){
+  if(!resetState){
+    if(strstr(outputBLE.c_str(), "OK+Set")){
+      Serial.println("Operation ble done, go with the next one");
+      bleOperationIndex++;
+    }
+  }
+}
+
+void checkResetBLE(){
+  if(strstr(outputBLE.c_str(), "OK+RESET")) {
+    Serial.println("BLE setup done.");
+    BLESerial.write("AT+NAME?");
+
+    setupBLEState = false;
+    bleAdvState = true;
+  }
+}
+
+void readBLE(){
+  if (BLESerial.available()) {
+    // String outputBLE = "";
+    Serial.print("HM10: ");
+    
+    outputBLE = BLESerial.readStringUntil('\n');
+    // prevMillis = millis();
+    // while (millis() - prevMillis < READ_TIME) {
+    //   if (BLESerial.available()) {
+    //     outputBLE += (char) BLESerial.read();
+    //   }
+    // }
+    Serial.println(outputBLE);
+    checkOkBLE();
+    checkResetBLE();
+  }  
+}
---
+++ state_machine_ble_slave/state_machine_ble_slave.ino
@@ -0,0 +1,90 @@
+#include <SoftwareSerial.h>
+#include <string.h>
+#include <YA_FSM.h>  // https://github.com/cotestatnt/YA_FSM
+
+#define NBIOTSerial Serial1
+#define powerPin 7 
+#define BLENAME "meter0"
+#define NUM_SETUPOPERATIONS_NBIoT 5
+#define NUM_SETUPOPERATIONS_BLE 11
+#define NUM_MASTEROPERATIONS_BLE 2
+
+const char *BLE_TAG = "BLE+";
+const char *NBIOT_TAG = "NB+";
+const char *RESET_NBIOT_TAG = "UFOTAS";
+/***** SERIAL READ VARS *****/
+unsigned long BAUD_RATE = 9600;
+const int BLUETOOTH_TX = 9;
+const int BLUETOOTH_RX = 8;
+const int READ_TIME = 500; //ms
+
+unsigned long prevMillis;
+
+SoftwareSerial BLESerial(BLUETOOTH_RX, BLUETOOTH_TX);
+
+String outputNBIOT;
+String outputBLE;
+int okNBIOTList = 0;
+int bleOperationIndex = 0;
+int oldStateBle = -1;
+String setupBLEList[NUM_SETUPOPERATIONS_BLE] = {"AT+IMME0", "AT+ROLE0", "AT+NAMEmeterSLAVE-104", "AT+NOTI1", "AT+NOTP1", "AT+ADTY0", "AT+BAUD0", "AT+ADVI1", "AT+SHOW3", "AT+PWRM1", "AT+RESET"};
+String masterBLEList[NUM_MASTEROPERATIONS_BLE] = {"AT+IMME1", "AT+ROLE1"};
+String cmd_ble;
+
+char *strremove(char *str, const char *sub);
+void readSerial();
+void readBLE();
+void readNBIOT();
+
+/***** FINITE STATE MACHINE VARS *****/
+YA_FSM stateMachine; // Create new FSM
+
+enum State {INIT, RESET, SETUP_BLE, BLE_ADV}; // State Alias
+const char * const stateName[] PROGMEM = {"INIT", "RESET", "SETUP_BLE", "BLE_ADV"}; // Helper for print labels instead integer when state change
+
+bool resetState = false;
+bool setupBLEState = false;
+bool setupNBIOTState = false;
+bool transmissionState = false;
+bool bleMasterState = false;
+bool bleAdvState = false;
+
+void setupStateMachine() {
+  stateMachine.AddState(stateName[INIT], nullptr, nullptr, onExit);
+  stateMachine.AddState(stateName[RESET], resetInterfaces, nullptr, onExit);
+  stateMachine.AddState(stateName[SETUP_BLE], onEnter, setupBlueToothConnection, onExit);
+  stateMachine.AddState(stateName[BLE_ADV], onEnter, bleAdvertisement, onExit);
+
+  // bool val at true activate the transition
+  stateMachine.AddTransition(RESET, SETUP_BLE, setupBLEState); 
+  stateMachine.AddTransition(SETUP_BLE, BLE_ADV, bleAdvState);    
+  stateMachine.AddTransition(INIT, RESET, resetState);
+}
+
+void setup() {
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin, HIGH);
+
+  Serial.begin(BAUD_RATE);
+  NBIOTSerial.begin(BAUD_RATE);
+  BLESerial.begin(BAUD_RATE);
+  delay(2000);
+
+  Serial.println("==== BLE Module started ====");
+  Serial.println();
+  Serial.println("Starting the Finite State Machine...");
+  setupStateMachine();
+  //switch from INIT to SETUP
+  resetState = true;
+}
+
+void loop() {
+  readSerial();
+  readBLE();
+  
+
+  if(stateMachine.Update()){
+    Serial.print(F("Active state: "));
+    Serial.println(stateMachine.ActiveStateName());
+  }
+}
